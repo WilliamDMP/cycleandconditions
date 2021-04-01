e75c001482b6831f8a44fd0118b6227617869589
William David Martinez Perez <wdmartinez89@gmail.com>
2021-04-01 12:42:04 -0500

    First commit

---
+++ main.js
@@ -0,0 +1,12 @@
+// this is a program that allows me to enter any given words to a list until i decide to stop it
+// first i need to create an empty array
+let list = [];
+// then i need to push elements to the array given by the user with a condition and a cycle
+while(true) {
+    let word = prompt('Insert an item to add to the list');
+    list.push(word);
+    console.log(list);
+    if(word === 'stop') {
+        break;
+    } 
+}
