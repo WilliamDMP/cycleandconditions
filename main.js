c5bd4500e8cf6ff658eaca3dbb7563411d64c9b7
William David Martinez Perez <wdmartinez89@gmail.com>
2021-04-03 10:37:00 -0500

    Using for

--- main.js
+++ main.js
@@ -1,9 +1,21 @@
-// this is a program that allows me to enter any given words to a list until i decide to stop it
-// first i need to create an empty array
+// // this is a program that allows me to enter any given words to a list until i decide to stop it
+// // first i need to create an empty array
+
+// // then i need to push elements to the array given by the user with a condition and a cycle
+// while(true) {
+//     let word = prompt('Insert an item to add to the list');
+//     if(word === 'stop') {
+//         break;
+//     } else {
+//         list.push(word);
+//         console.log(list);    
+//     }
+// }
+
+// using for 
 let list = [];
-// then i need to push elements to the array given by the user with a condition and a cycle
-while(true) {
-    let word = prompt('Insert an item to add to the list');
+    for (let i = 0; i <= list.length; i++) {
+        let word = prompt('Insert an item to add to the list');
     if(word === 'stop') {
         break;
     } else {
@@ -11,3 +23,5 @@ while(true) {
         console.log(list);    
     }
 }
+    
+
